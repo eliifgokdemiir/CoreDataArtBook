684a3d74c387d14890dd774c876c149687c3a899
elifgokdemir <gokdemirelif12@gmail.com>
2024-11-25 13:05:09 +0300

    Initial Commit

---
+++ CoreDataArtBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataArtBook
+//
+//  Created by Elif Gökdemir on 25.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
